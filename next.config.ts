5af800f948dad362690362836124467389c39a66
Vemula Sai Mahisrith <sai.mahisrithv@outlook.com>
2026-04-19 15:39:03 +0530

    removed eslint

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Disables ESLint errors from breaking deploys
-  },
+
 }
 
 export default nextConfig
